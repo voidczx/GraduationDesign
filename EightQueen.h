72c0a3cc088f4cd97cad9b7c8f26429b982d5b28
aa <a>
2022-03-08 23:55:00 +0800

    EightQueen 逻辑类创建

---
+++ EightQueen.h
@@ -0,0 +1,23 @@
+#ifndef EIGHTQUEEN_H
+#define EIGHTQUEEN_H
+
+#include "vector"
+
+struct QueenChess;
+
+class EightQueen{
+
+public:
+
+
+
+private:
+
+    std::vector<std::vector<bool>> Map;
+};
+
+struct QueenChess{
+
+};
+
+#endif // EIGHTQUEEN_H
