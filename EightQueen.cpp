4c9debc63f1db690490135925f190ea04061e6ac
aa <a>
2022-03-10 23:48:51 +0800

    EightQueen 逻辑

--- EightQueen.cpp
+++ EightQueen.cpp
@@ -1,3 +1,73 @@
 #include "EightQueen.h"
 
+EightQueen::EightQueen(const uint8_t& MapSize)
+    : Map(MapSize, std::vector<bool>(MapSize, true)), MapSize(MapSize){
+
+}
+
+bool EightQueen::IsPositionValid(const uint8_t& Row, const uint8_t& Col) const{
+    if (Row >= MapSize || Col >= MapSize){
+        return false;
+    }
+    if (!Map[Row][Col]){
+        return false;
+    }
+    return true;
+}
+
+bool EightQueen::TryAddQueenChess(const uint8_t& Row, const uint8_t& Col){
+    if (!IsPositionValid(Row, Col)){
+        return false;
+    }
+    QueenChess NewQueenChess(Row, Col);
+    ChessArray.push_back(NewQueenChess);
+    AfterAddQueenChess();
+    return true;
+}
+
+bool EightQueen::TryReduceQueenChess(){
+    if (ChessArray.size() <= 0){
+        return false;
+    }
+    BeforeReduceQueenChess();
+    ChessArray.pop_back();
+    return true;
+}
+
+void EightQueen::AfterAddQueenChess(){
+    uint8_t Row = ChessArray.back().Row;
+    uint8_t Col = ChessArray.back().Col;
+    const uint8_t BiasFlag = Row - Col;
+    const uint8_t AntiBiasFlag = Row + Col;
+    uint8_t ResPositionNum = 0;
+    for (uint8_t RowIndex = 0; RowIndex < MapSize; RowIndex++){
+        for (uint8_t ColIndex = 0; ColIndex < MapSize; ColIndex++){
+            if (RowIndex == Row || ColIndex == Col || RowIndex - ColIndex == BiasFlag || RowIndex + ColIndex == AntiBiasFlag){
+                if (Map[RowIndex][ColIndex]){
+                    Map[RowIndex][ColIndex] = false;
+                    MapUnit NewMapUnit(RowIndex, ColIndex);
+                    ChessArray.back().EffectUnitArray.push_back(NewMapUnit);
+                }
+            }
+            if (Map[RowIndex][ColIndex]){
+                ResPositionNum++;
+            }
+        }
+    }
+    if (ChessArray.size() >= MapSize){
+        bSuccess = true;
+        return;
+    }
+    if (ResPositionNum <= 0){
+        bFail = true;
+        return;
+    }
+}
+
+void EightQueen::BeforeReduceQueenChess(){
+    for (const MapUnit& Unit : ChessArray.back().EffectUnitArray){
+        Map[Unit.Row][Unit.Col] = true;
+    }
+    Map[ChessArray.back().Row][ChessArray.back().Col] = true;
+}
 
--- EightQueen.h
+++ EightQueen.h
@@ -2,6 +2,7 @@
 #define EIGHTQUEEN_H
 
 #include "vector"
+#include "deque"
 #include "stdint.h"
 
 struct QueenChess;
@@ -12,16 +13,43 @@ public:
 
     EightQueen(const uint8_t& MapSize);
 
+    bool IsPositionValid(const uint8_t& Row, const uint8_t& Col) const;
+    bool TryAddQueenChess(const uint8_t& Row, const uint8_t& Col);
+    bool TryReduceQueenChess();
+
+    bool IsSuccess() const { return bSuccess; }
+    bool IsFail() const { return bFail; }
 
 private:
 
+    void AfterAddQueenChess();
+    void BeforeReduceQueenChess();
+
     std::vector<std::vector<bool>> Map;
-    std::vector<QueenChess> ChessArray;
+    std::deque<QueenChess> ChessArray;
+
+    const uint8_t MapSize;
+    bool bSuccess = false;
+    bool bFail = false;
+};
+
+struct MapUnit{
+
+    MapUnit() : Row(0), Col(0) {};
+    MapUnit(const uint8_t& InRow, const uint8_t& InCol) : Row(InRow), Col(InCol) {};
+
+    uint8_t Row;
+    uint8_t Col;
 };
 
 struct QueenChess{
+
+    QueenChess() : Row(0), Col(0) {};
+    QueenChess(const uint8_t& InRow, const uint8_t& InCol) : Row(InRow), Col(InCol) {};
+
     uint8_t Row;
     uint8_t Col;
+    std::vector<MapUnit> EffectUnitArray;
 };
 
 #endif // EIGHTQUEEN_H
