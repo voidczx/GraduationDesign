69d469fa0ee9a4663787fd200d0936c0b8fdf8e5
aa <a>
2022-03-25 01:05:08 +0800

    HuffmanEncoding - 逻辑 StepForward 部分

--- HuffmanEncoding.cpp
+++ HuffmanEncoding.cpp
@@ -2,38 +2,256 @@
 
 HuffmanEncoding::HuffmanEncoding()
 {
-
+    Start = new FrequencyListNode();
+    Root = nullptr;
+    TreeIter = nullptr;
 }
 
-const std::vector<HuffmanEncoding::Process> HuffmanEncoding::GenerateFrequencyMap(const std::string& InStr){
+HuffmanEncoding::~HuffmanEncoding(){
+    DeleteFrequencyList(Start);
+    Start = nullptr;
+    DeleteHuffmanTree(Root);
+    Root = nullptr;
+    TreeIter = nullptr;
+}
 
-    std::vector<HuffmanEncoding::Process> Output;
+const std::unordered_map<char, int32_t> HuffmanEncoding::GenerateFrequencyMap(const std::string& InStr){
+
+    std::unordered_map<char, int32_t> Output;
+    for (const char& Ch : InStr){
+        if (Output.count(Ch) == 0){
+            Output.insert(std::pair<char, int32_t>(Ch, 1));
+        }
+        else{
+            Output[Ch]++;
+        }
+    }
+    for (const std::pair<const char, int32_t>& Pair : Output){
+        std::string Word = "";
+        Word.push_back(Pair.first);
+        AddFrequencyNode(Word, Pair.second);
+    }
+    return Output;
+}
+
+const std::vector<std::shared_ptr<HuffmanEncoding::Process>> HuffmanEncoding::StepForward(){
+
+    std::vector<std::shared_ptr<HuffmanEncoding::Process>> Output;
+    if (bBinaryBuildCompleted){
+        return Output;
+    }
+
+    if (!bTreeBuildCompleted){
+        // Tree Node Process
+        FrequencyListNode* FirstNode = Start->Next;
+        FrequencyListNode* SecondNode = FirstNode->Next;
+        if (FirstNode == nullptr || SecondNode == nullptr){
+            // Error!
+            return Output;
+        }
+
+        // Add Process
+        std::string AddWord = FirstNode->Word + "/" + SecondNode->Word;
+        int32_t AddValue = FirstNode->Value + SecondNode->Value;
+        std::shared_ptr<TreeNodeProcess> AddNodeProcess(new TreeNodeProcess());
+        AddNodeProcess->Str = AddWord;
+        AddNodeProcess->Frequency = AddValue;
+        Output.push_back(AddNodeProcess);
+
+        // Remove Process
+        std::shared_ptr<TreeNodeProcess> RemoveFirstNodeProcess(new TreeNodeProcess());
+        RemoveFirstNodeProcess->Str = FirstNode->Word;
+        Output.push_back(RemoveFirstNodeProcess);
+        std::shared_ptr<TreeNodeProcess> RemoveSecondNodeProcess(new TreeNodeProcess());
+        RemoveSecondNodeProcess->Str = SecondNode->Word;
+        Output.push_back(RemoveSecondNodeProcess);
+
+        // Add Nodes
+        AddFrequencyNode(AddWord, AddValue);
+        HuffmanTreeNode* Parent = new HuffmanTreeNode();
+        Root = Parent;
+        Parent->Word = AddWord;
+        HuffmanTreeNode* LeftChild = new HuffmanTreeNode();
+        LeftChild->Word = FirstNode->Word;
+        LeftChild->Parent = Parent;
+        Parent->LeftChild = LeftChild;
+        HuffmanTreeNode* RightChild = new HuffmanTreeNode();
+        RightChild->Word = SecondNode->Word;
+        RightChild->Parent = Parent;
+        Parent->RightChild = RightChild;
+
+        // Remove Nodes
+        RemoveFrequencyNode(FirstNode->Word);
+        FirstNode = nullptr;
+        RemoveFrequencyNode(SecondNode->Word);
+        SecondNode = nullptr;
+
+        // Judge Completed
+        if (Start->Next == nullptr){
+            // Error!
+            return Output;
+        }
+        if (Start->Next->Next == nullptr){
+            bTreeBuildCompleted = true;
+        }
+    }
+    else{
+        // Binary Process
+        if (Root == nullptr || Root->LeftChild == nullptr || Root->RightChild == nullptr){
+            // Error or Empty
+            return Output;
+        }
+
+        // Encoding Current Node And Add Process
+        if (TreeIter == nullptr){
+            TreeIter = Root->LeftChild;
+            TreeIter->EncodingResult = "0";
+            std::shared_ptr<BinaryProcess> StartBinaryBuildProcess(new BinaryProcess());
+            StartBinaryBuildProcess->Type = ProcessType::EAdd;
+            StartBinaryBuildProcess->StrSource = Root->Word;
+            StartBinaryBuildProcess->StrTarget = TreeIter->Word;
+            StartBinaryBuildProcess->Value = "0";
+            Output.push_back(StartBinaryBuildProcess);
+        }
+        else{
+            if (TreeIter->Parent == nullptr){
+                // Error
+                return Output;
+            }
+            std::shared_ptr<BinaryProcess> AddLineProcess(new BinaryProcess());
+            AddLineProcess->Type = ProcessType::EAdd;
+            AddLineProcess->StrSource = TreeIter->Parent->Word;
+            AddLineProcess->StrTarget = TreeIter->Word;
+            if (TreeIter == TreeIter->Parent->LeftChild){
+                TreeIter->EncodingResult = TreeIter->Parent->EncodingResult + "0";
+                AddLineProcess->Value = "0";
+            }
+            else{
+                TreeIter->EncodingResult = TreeIter->Parent->EncodingResult + "1";
+                AddLineProcess->Value = "1";
+            }
+            Output.push_back(AddLineProcess);
+        }
+
+        // Find Next Node
+        if (TreeIter->LeftChild != nullptr){
+            TreeIter = TreeIter->LeftChild;
+        }
+        else{
+            while (TreeIter->Parent != nullptr){
+                TreeIter = TreeIter->Parent;
+                if (TreeIter->EncodingResult == ""){
+                    TreeIter = TreeIter->RightChild;
+                    break;
+                }
+            }
+            if (TreeIter == Root){
+                if (Root->RightChild->EncodingResult == ""){
+                    TreeIter = Root->RightChild;
+                }
+                else{
+                    bBinaryBuildCompleted = true;
+                    TreeIter = nullptr;
+                }
+            }
+        }
+    }
+    ProcessStack.push_back(Output);
     return Output;
 
 }
 
-const std::vector<HuffmanEncoding::Process> HuffmanEncoding::StepForward(){
+const std::vector<std::shared_ptr<HuffmanEncoding::Process>> HuffmanEncoding::StepBack(){
 
-    std::vector<HuffmanEncoding::Process> Output;
+    std::vector<std::shared_ptr<HuffmanEncoding::Process>> Output;
     return Output;
 
 }
 
-const std::vector<HuffmanEncoding::Process> HuffmanEncoding::StepBack(){
+bool HuffmanEncoding::RemoveFrequencyNode(const std::string& InStr){
 
-    std::vector<HuffmanEncoding::Process> Output;
-    return Output;
+    FrequencyListNode* ListNode = Start;
+    if (ListNode == nullptr){
+        return false;
+    }
+    while (ListNode != nullptr && ListNode->Word != InStr){
+        ListNode = ListNode->Next;
+    }
+    if (ListNode == nullptr || ListNode->Front == nullptr){
+        return false;
+    }
+    ListNode->Front->Next = ListNode->Next;
+    if (ListNode->Next != nullptr){
+        ListNode->Next->Front = ListNode->Front;
+    }
+    ListNode->Front = nullptr;
+    ListNode->Next = nullptr;
+    delete ListNode;
+    return true;
+}
+
+bool HuffmanEncoding::AddFrequencyNode(const std::string& InStr, const int32_t& InFrequency){
+
+    FrequencyListNode* ListNode = Start;
+    if (ListNode == nullptr){
+        return false;
+    }
+    while (ListNode->Next != nullptr && ListNode->Next->Value < InFrequency){
+        ListNode = ListNode->Next;
+    }
+    FrequencyListNode* NewNode = new FrequencyListNode();
+    NewNode->Word = InStr;
+    NewNode->Value = InFrequency;
+    NewNode->Next = ListNode->Next;
+    if (NewNode->Next != nullptr){
+        NewNode->Next->Front = NewNode;
+    }
+    NewNode->Front = ListNode;
+    ListNode->Next = NewNode;
+    return true;
 
 }
 
-bool RemoveFrequency(const std::string& InStr){
+bool HuffmanEncoding::DeleteFrequencyList(FrequencyListNode* InDeletedNode){
 
-    return false;
+    if (InDeletedNode == nullptr){
+        return false;
+    }
+    if (InDeletedNode->Next != nullptr){
+        DeleteFrequencyList(InDeletedNode->Next);
+    }
+    if (InDeletedNode->Front != nullptr){
+        InDeletedNode->Front->Next = nullptr;
+    }
+    delete InDeletedNode;
+    return true;
 
 }
 
-bool AddFrequency(const std::string& InStr, const int32_t& InFrequency){
+bool HuffmanEncoding::DeleteHuffmanTree(HuffmanTreeNode* InDeletedNode){
 
-    return false;
+    if (InDeletedNode == nullptr){
+        return false;
+    }
+    if (InDeletedNode->LeftChild != nullptr){
+        DeleteHuffmanTree(InDeletedNode->LeftChild);
+    }
+    if (InDeletedNode->RightChild != nullptr){
+        DeleteHuffmanTree(InDeletedNode->RightChild);
+    }
+    if (InDeletedNode->Parent != nullptr){
+        if (InDeletedNode->Parent->LeftChild == InDeletedNode){
+            InDeletedNode->Parent->LeftChild = nullptr;
+        }
+        else if (InDeletedNode->Parent->RightChild == InDeletedNode){
+            InDeletedNode->Parent->RightChild = nullptr;
+        }
+        else{
+            // Error!
+        }
+    }
+    delete InDeletedNode;
+    return true;
 
 }
+
--- HuffmanEncoding.h
+++ HuffmanEncoding.h
@@ -6,51 +6,103 @@
 #include "string"
 #include "vector"
 #include "deque"
+#include "memory"
 
 class HuffmanEncoding
 {
 
 public:
 
-    enum ProcessType{
+    enum class ProcessClass{
+        ENone = 0,
+        ETreeNode = 1,
+        EBinary = 2,
+    };
+
+    enum class ProcessType{
         ENone = 0,
         EAdd = 1,
         ERemove = 2,
     };
 
-    struct FrequencyInfo{
+    struct HuffmanTreeNode{
 
-        FrequencyInfo() : Value(-1), Next(std::string()) {}
+        HuffmanTreeNode() : Word(), Parent(nullptr), LeftChild(nullptr), RightChild(nullptr), EncodingResult() {}
 
+        std::string Word;
+        HuffmanTreeNode* Parent;
+        HuffmanTreeNode* LeftChild;
+        HuffmanTreeNode* RightChild;
+        std::string EncodingResult;
+    };
+
+    struct FrequencyListNode{
+
+        FrequencyListNode() : Word(), Value(-1), Next(nullptr), Front(nullptr) {}
+
+        std::string Word;
         int32_t Value;
-        std::string Next;
+        FrequencyListNode* Next;
+        FrequencyListNode* Front;
+        HuffmanTreeNode* TreeNode;
     };
 
     struct Process{
 
-        Process() : Type(ProcessType::ENone), Str(std::string()), Frequency(-1) {}
+        Process() : Class(ProcessClass::ENone) {}
+        Process(ProcessClass InClass) : Class(InClass) {}
+
+        ProcessClass Class;
+    };
+
+    struct TreeNodeProcess : public Process{
+
+        TreeNodeProcess() : Process(ProcessClass::ETreeNode), Type(ProcessType::ENone), Str(), Frequency(-1) {}
 
         ProcessType Type;
         std::string Str;
         int32_t Frequency;
     };
 
+    struct BinaryProcess : public Process{
+
+        BinaryProcess() : Process(ProcessClass::EBinary), Type(ProcessType::ENone), StrSource(), StrTarget(), Value() {}
+
+        ProcessType Type;
+        std::string StrSource;
+        std::string StrTarget;
+        std::string Value;
+    };
+
 public:
 
     HuffmanEncoding();
+    ~HuffmanEncoding();
 
-    const std::vector<Process> GenerateFrequencyMap(const std::string& InStr);
-    const std::vector<Process> StepForward();
-    const std::vector<Process> StepBack();
+    bool IsTreeBuildCompleted() const { return bTreeBuildCompleted; }
+    bool IsBinaryBuildCompleted() const { return bBinaryBuildCompleted; }
+    std::string GetEncodingResult(const std::string& InStr) { return EncodingMap.count(InStr) > 0 ? EncodingMap[InStr] : ""; }
+
+    const std::unordered_map<char, int32_t> GenerateFrequencyMap(const std::string& InStr);
+    const std::vector<std::shared_ptr<Process>> StepForward();
+    const std::vector<std::shared_ptr<Process>> StepBack();
 
 
 private:
 
-    bool RemoveFrequency(const std::string& InStr);
-    bool AddFrequency(const std::string& InStr, const int32_t& InFrequency);
+    bool RemoveFrequencyNode(const std::string& InStr);
+    bool AddFrequencyNode(const std::string& InStr, const int32_t& InFrequency);
 
-    std::unordered_map<std::string, FrequencyInfo> FrequencyMap;
-    std::deque<Process> ProcessStack;
+    bool DeleteFrequencyList(FrequencyListNode* InDeletedNode);
+    bool DeleteHuffmanTree(HuffmanTreeNode* InDeletedNode);
+
+    FrequencyListNode* Start;
+    HuffmanTreeNode* Root;
+    HuffmanTreeNode* TreeIter;
+    std::deque<std::vector<std::shared_ptr<Process>>> ProcessStack;
+    std::unordered_map<std::string, std::string> EncodingMap;
+    bool bTreeBuildCompleted = false;
+    bool bBinaryBuildCompleted = false;
 
 };
 
